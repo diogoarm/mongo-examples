d946008d69e85b7c03b9daaf54c1611d0fb938c8
Diogo Monteiro <diogo.arm@gmail.com>
2015-09-02 21:19:30 -0300

    creating verticle

---
+++ src/main/java/io/vertx/examples/mongo/MongoClientVerticle.java
@@ -0,0 +1,19 @@
+package io.vertx.examples.mongo;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by diogoam on 02/09/2015.
+ */
+public class MongoClientVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
+}
